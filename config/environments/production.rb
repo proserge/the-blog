f268882690fd06d41379ea7fc0264a7c28bfcc2e
proserge <34370028+proserge@users.noreply.github.com>
2018-12-17 23:52:51 +0200

    Send mail changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,24 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "the-blog_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  #config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = {:host => 'the-blog-2.herokuapp.com', :protocol => 'http'} #I've also tried it without ":protocol => 'http'"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :authentication => :plain,   # I've also tried :login
+    :enable_starttls_auto => true,  # Also tried tls => true
+    :user_name => 'noreply4831@gmail.com',
+    :password => 't3stp4ss'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
