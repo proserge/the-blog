e26ac9482e176209748e354d1990b840a81f2cb5
proserge <34370028+proserge@users.noreply.github.com>
2019-01-27 22:54:48 +0200

    Add Comments functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   validates :title, :body, presence: true
   validates :blog_id, presence: true
   belongs_to :blog
+  has_many :comments, dependent: :destroy
   #belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :blogs do
-    resources :posts, shallow: true
+  resources :blogs #do
+  resources :posts do #, shallow: true
+    resources :comments, only: [:new, :create, :index, :destroy]
   end
 end
